fdb234ac90840b21d053452e2ad5b772016d7bfd
phillychi3 <55632143+phillychi3@users.noreply.github.com>
2021-02-10 10:42:18 +0800

    030

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ with open("lol", "r") as lo:
     long_description = lo.read()
 
 setuptools.setup(
-     name='youtube_live_status',  
-     version='0.1.1',
+     name='youtube_lv',  
+     version='0.0.1',
      author="phillychi3",
      author_email="phillychi3@gmail.com",
      description="check youtuber live status",
      long_description=long_description,
      url="https://github.com/phillychi3/youtube_live_status",
-     py_modules=["youtube","youtuber","status"],
+     py_modules=["youtube","youtuber","status","live"],
      classifiers=[
          "Programming Language :: Python :: 3",
          "License :: OSI Approved :: Apache Software License",
