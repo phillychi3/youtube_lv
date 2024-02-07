42a5e0078a2a859ccc7b3abdb8e451509c74cbce
phillychi3 <phillychi3@gmail.com>
2024-02-07 23:18:38 +0800

    feat: add more test

--- tests/test_twitch.py
+++ tests/test_twitch.py
@@ -4,3 +4,9 @@ from ytlv import twitch
 def test_get_true_func():
     live=twitch("leekbeats")
     assert  live.status =="LIVE"
+
+def test_twitch_url_true():
+    try:
+        live=twitch("https://www.twitch.tv/utonyan")  # noqa: F841
+    except:  # noqa: E722
+        assert False
