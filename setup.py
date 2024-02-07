72674487ae51b0611e9741e69562cf32ca4363af
phillychi3 <phillychi3@gmail.com>
2024-02-07 23:59:53 +0800

    update tag

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='youtube_lv',  
-    version='1.1.0',
+    name='youtube_lv',
+    version='1.2.0',
     author="phillychi3",
     author_email="phillychi3@gmail.com",
     description="check youtuber live status",
     long_description=long_description,
-    long_description_content_type="text/markdown",     
+    long_description_content_type="text/markdown",
     url="https://github.com/phillychi3/youtube_lv",
     py_modules=["youtube","youtuber","status","live"],
     packages=setuptools.find_packages(),
